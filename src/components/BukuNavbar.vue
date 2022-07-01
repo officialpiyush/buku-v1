cfda3da0f3b4e24df28d77e4bf504c42eb4590d5
Piyush <bhangalepiyush@gmail.com>
2022-07-01 17:10:47 +0530

    feat: remove unused icons and seperate icons

--- src/components/BukuNavbar.vue
+++ src/components/BukuNavbar.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import PlusIcon from "./icons/PlusIcon.vue"
+</script>
+
+
 <template>
     <div class="bg-gray-100 w-full">
         <div class="py-4 lg:(py-4) px-4 w-full max-w-6xl mx-auto flex items-center justify-between">
@@ -8,11 +13,7 @@
             <div class="flex items-center gap-4" id="navbar_end">
                 <router-link disabled="false" to="/new"
                     class="flex items-center gap-1 rounded-md px-4 py-1 lg:(py-2) cursor-pointer hover:(bg-green-600 text-light-100) bg-green-500 text-green-100">
-                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <path fill="none" d="M0 0h24v24H0z" />
-                        <path
-                            d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-1-11H7v2h4v4h2v-4h4v-2h-4V7h-2v4z" />
-                    </svg>
+                    <PlusIcon />
                     <span>New Book</span>
                 </router-link>
                 <!-- <router-link to="https://github.com/officialpiyush">Github</router-link> -->
